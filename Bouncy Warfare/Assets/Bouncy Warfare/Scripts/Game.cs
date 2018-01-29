c346b6c63aab6efacba4eb4d4565dc570ae10c0b
James Walsh <jameswalsh6661@hotmail.com>
2018-01-29 17:34:36 +0000

    Update. Code

--- Bouncy Warfare/Assets/Bouncy Warfare/Scripts/Game.cs
+++ Bouncy Warfare/Assets/Bouncy Warfare/Scripts/Game.cs
@@ -77,7 +77,8 @@ public class Game : MonoBehaviour
             WinGame(0);	//Player 1 wins the game.
             
 		}		
-		if(player2Score >= maxScore){	//Does player 2 reach the score amount to win the game?
+		if(player2Score >= maxScore)
+		{	//Does player 2 reach the score amount to win the game?
 		    WinGame(1);					//Player 2 wins the game.
 		}
 	}
@@ -101,14 +102,18 @@ public class Game : MonoBehaviour
 	{
 		string[] lines = map.Split("\n"[0]);	//Splits the file into seperate lines, each indicating a seperate tile.
 
-		for(int x = 0; x < lines.Length; x++){				//Loop through all the tiles.
-			if(lines[x] != ""){								//Is the line not blank?
+		for(int x = 0; x < lines.Length; x++)
+		{				//Loop through all the tiles.
+			if(lines[x] != "")
+			{								//Is the line not blank?
 				string[] parts = lines[x].Split(","[0]);	//Then split that line at every comma.
 
-				if(parts[0].Contains("Wall")){				//Is this tile a walll?
+				if(parts[0].Contains("Wall"))
+				{				//Is this tile a walll?
 					GameObject wall = Instantiate(wallPrefab, new Vector3(float.Parse(parts[1]), float.Parse(parts[2]), 0), Quaternion.identity) as GameObject;	//Spawn in the wall game object.
 				}
-				else if(parts[0].Contains("SpawnPoint")){	//Is this tile a spawn point?
+				else if(parts[0].Contains("SpawnPoint"))
+				{	//Is this tile a spawn point?
 					GameObject spawnPoint = new GameObject("SpawnPoint");	//Spawn a blank game object which will be the spawn point.
 					spawnPoint.transform.position = new Vector3(float.Parse(parts[1]), float.Parse(parts[2]), 0);	//Set the spawn point position.
 					spawnPoints.Add(spawnPoint);			//Add the spawn point to the spawnPoints list.
